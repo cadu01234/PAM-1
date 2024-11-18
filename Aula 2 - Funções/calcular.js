23faacc73f82733340f9b85fca63bd261fb77630
Carlos Silva <145131105+cadu01234@users.noreply.github.com>
2024-11-18 10:24:49 -0300

    calcular.js

---
+++ Aula 2 - Funções/calcular.js
@@ -0,0 +1,20 @@
+function soma (num1, num2) {
+    return num1+num2;
+}
+
+function subtracao (num1, num2) {
+    return num1-num2;
+}
+
+function divisao (num1, num2) {
+    return num1/num2;
+}
+
+function multiplicacao (num1, num2) {
+    return num1*num2;
+}
+
+console.log(soma(2,5));
+console.log(subtracao(5, 5));
+console.log(divisao(4, 2));
+console.log(multiplicacao(5, 2));
