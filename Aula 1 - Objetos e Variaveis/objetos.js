f4c93203e21d4f12675104627e15344b465ffdbd
Carlos Silva <145131105+cadu01234@users.noreply.github.com>
2024-11-18 10:21:53 -0300

    objetos.js

---
+++ Aula 1 - Objetos e Variaveis/objetos.js
@@ -0,0 +1,44 @@
+// Objetos
+
+var aluno1 = {
+    matricula: 7627364,
+    nome: "Danilo Ferreria",
+    curso: "Bootcamp front end",
+    ativo: true
+};
+
+console.log(aluno1.nome)
+
+aluno1.dataNascimento = '03/07/1985'
+console.log(aluno1.dataNascimento)
+
+delete aluno1.dataNascimento;
+
+// Undefined e null
+
+var x;
+var y = null;
+
+// Arrays 
+
+var frutas = ['Banana', 'Laranja', 'Maçã']
+
+var jogador1 = {
+    nome: "João Pedro",
+    camisa: "10",
+    clube: "São Paulo",
+    salario: "1 bilhão",
+    convocado: true
+};
+
+var jogador2 = {
+    nome: "Carlos Eduardo",
+    camisa: "9",
+    clube: "São Paulo",
+    salario: "1 bilhão",
+    convocado: true
+};
+
+console.log(jogador1)
+console.log(jogador2)
+    
